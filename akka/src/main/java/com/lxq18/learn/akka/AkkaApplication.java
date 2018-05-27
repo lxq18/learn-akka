b9bfd5bb3413a42c4b72e08b3e8c5acbb0604293
xqli <itxeli@126.com>
2018-05-27 12:24:27 +0800

    init project

---
+++ akka/src/main/java/com/lxq18/learn/akka/AkkaApplication.java
@@ -0,0 +1,12 @@
+package com.lxq18.learn.akka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkaApplication.class, args);
+	}
+}
